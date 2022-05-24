ef4403eab6f1d8ea1878090ebd8d4ee202ae50e9
Olivia jingyao <oliviachen797@gmail.com>
2022-05-25 00:43:28 +1000

    自动播放

--- 10-爱旅行/js/index.js
+++ 10-爱旅行/js/index.js
@@ -29,4 +29,26 @@
         $('.sugform').slideUp()
     })
 
+
+
+    //自动轮播
+    //拿到最后一张图片 返回的是jquery对象 所以用$开头
+    function autoPlay() {
+        let $last = $('.wblist').last()
+        let wbdesc = $('.wbdesc')
+
+        //放到父元素的开头
+        wbdesc.prepend($last)
+            //把图片向上移动到$last的高度  outerHeight(true)是带margin的高度
+        let height = $last.outerHeight(true)
+            //往上走是-height
+        wbdesc.css('top', -height)
+            //添加动画延迟 因为不是立即播放的  改变的属性就是-height 往下移动所以是0
+        wbdesc.delay(2000).animate({
+            top: 0
+        }, function() {
+            autoPlay()
+        })
+    }
+    autoPlay()
 })()
