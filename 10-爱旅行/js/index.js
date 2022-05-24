dcaecef20085f964d40c8357249fcb4fa6a858db
Olivia jingyao <oliviachen797@gmail.com>
2022-05-24 23:29:10 +1000

    3.点击意见箱suggest展开盒子 4.点击close按钮收起盒子

--- 10-爱旅行/js/index.js
+++ 10-爱旅行/js/index.js
@@ -1,22 +1,32 @@
 ;
 (function() {
-    //默认隐藏返回顶部
+    //1.默认隐藏返回顶部
     $('.gotop').hide()
         //注册滚动事件
     $(window).scroll(function() {
-        let top = $('html').scrollTop()
-        if (top > 200) {
-            //滚动快了不会没有显示 stop()
-            $('.gotop').stop().fadeIn()
-        } else {
-            $('.gotop').stop().fadeOut()
+            let top = $('html').scrollTop()
+            if (top > 200) {
+                //滚动快了不会没有显示 stop()
+                $('.gotop').stop().fadeIn()
+            } else {
+                $('.gotop').stop().fadeOut()
 
-        }
+            }
+        })
         // 2.点击返回顶部
-        $('.gotop').click(function() {
-            $('html').animate({
-                scrollTop: 0
-            })
+    $('.gotop').click(function() {
+        $('html').animate({
+            scrollTop: 0
         })
     })
+
+    //3.点击展开/收起右侧盒子
+    $('.suggest').click(function() {
+            $('.sugform').slideDown()
+        })
+        //点击关闭 关闭盒子
+    $('.close').click(function() {
+        $('.sugform').slideUp()
+    })
+
 })()
